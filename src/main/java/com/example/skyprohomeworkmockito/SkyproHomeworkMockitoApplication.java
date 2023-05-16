fa9b06d28ed78cab79bd02a7e386654e923f050b
A.Solovyov <Soloviev.gvardiya@gmail.com>
2023-05-16 14:30:35 +0300

    Первый коммит

---
+++ src/main/java/com/example/skyprohomeworkmockito/SkyproHomeworkMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.example.skyprohomeworkmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproHomeworkMockitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyproHomeworkMockitoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/skyprohomeworkmockito/SkyproHomeworkMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skyprohomeworkmockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproHomeworkMockitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
